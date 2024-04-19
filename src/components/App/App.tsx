d1c58f159234db35bf380b1a939b28038c62da30
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-19 19:13:36 +0300

    ADD | upload_to_openai.js

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   console.log('App');
 
-  return <div className='App'>Github Actions 18</div>;
+  return <div className='App'>Github Actions 19</div>;
 };
 
 export default App;
