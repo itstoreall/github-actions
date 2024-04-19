fed9443555613540ae11fca000d088777dcb5392
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-19 18:42:40 +0300

    EDIT | Move the py file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   console.log('App');
 
-  return <div className='App'>Github Actions 13</div>;
+  return <div className='App'>Github Actions 14</div>;
 };
 
 export default App;
