ca95e14506a21af7042eaaf51834f16d22651907
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-20 14:11:31 +0300

    EDIT | js 26

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   console.log('App');
 
-  return <div className='App'>Github Actions 25</div>;
+  return <div className='App'>Github Actions 26</div>;
 };
 
 export default App;
