52f0b8afc0f506bbd2a43b0e0fc1dd9dcf57e047
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-20 13:26:15 +0300

    EDIT | Github Actions 22

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 const App = () => {
   console.log('App');
 
-  return <div className='App'>Github Actions 21</div>;
+  return <div className='App'>Github Actions 22</div>;
 };
 
 export default App;
