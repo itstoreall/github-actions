17db387887cbf9f3cf8e6d85360d3e4f93333ea0
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-21 16:03:46 +0300

    EDIT | 55

--- codeReviewHandler.js
+++ codeReviewHandler.js
@@ -1,39 +1,37 @@
 const axios = require('axios');
 // const sendToGPTForReview = require('./sendToGPTForReview');
 
-const codeReviewHandler = async directoryPath => {
-  // module.exports = async function codeReviewHandler(directoryPath) {
-  console.log(`codeReviewHandler fn!!!`);
-  console.log(`Reviewing files in directory -> ${directoryPath}`);
+// const codeReviewHandler = async directoryPath => {
+// module.exports = async function codeReviewHandler(directoryPath) {
+console.log(`codeReviewHandler fn!!!`);
+// console.log(`Reviewing files in directory -> ${directoryPath}`);
 
-  // ------ Date
+// ------ Date
 
-  const dateUkraine = new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'long',
-    day: '2-digit',
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-    timeZone: 'Europe/Kyiv'
-  }).format(new Date());
+const dateUkraine = new Intl.DateTimeFormat('en-US', {
+  year: 'numeric',
+  month: 'long',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  timeZone: 'Europe/Kyiv'
+}).format(new Date());
 
-  console.log(`new Date() --------->: ${dateUkraine}`);
+console.log(`new Date() --------->: ${dateUkraine}`);
 
-  // ------ Mock Data
+// ------ Mock Data
 
-  const apiUrl = `https://jsonplaceholder.typicode.com/todos/1`;
+const apiUrl = `https://jsonplaceholder.typicode.com/todos/1`;
 
-  try {
-    const response = await axios.get(apiUrl);
-    console.log('axios ------------>', response.data.title);
-  } catch (error) {
-    console.error('Failed:', error);
-  }
+try {
+  const response = await axios.get(apiUrl);
+  console.log('axios ------------>', response.data.title);
+} catch (error) {
+  console.error('Failed:', error);
+}
 
-  // sendToGPTForReview();
-};
-
-export default codeReviewHandler;
+// sendToGPTForReview();
+// };
 
 // codeReviewHandler();
