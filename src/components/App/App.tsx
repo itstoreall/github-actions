08e9094859c27232d8e22190e644bab2cb1e6880
Serhii Stanislav <serhiistanislav@gmail.com>
2024-04-22 15:34:40 +0300

    EDIT | 94

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,7 @@ const App = () => {
   console.log('App');
   // console.log('App', codeReviewHandler());
 
-  return <div className='App'>Github Actions 93</div>;
+  return <div className='App'>Github Actions 94</div>;
 };
 
 export default App;
